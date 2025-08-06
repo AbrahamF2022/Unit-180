8539725ee1cf3afc53c332b36f84d5417d6baf14
Abraham Flynn <flynnabe@gmail.com>
2025-08-05 22:01:46 -0700

    Move Emmanuel's picture higher - center -25%

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -466,12 +466,12 @@ const Index = () => {
                   <div className="relative z-10 flex flex-col md:flex-row items-center md:items-start gap-6 md:gap-8">
                     {/* Emanuel Smith Image */}
                     <div className="w-32 h-32 md:w-40 md:h-40 rounded-2xl overflow-hidden shadow-2xl border-4 border-white/20 group-hover:border-green-300/50 transition-all duration-500 group-hover:scale-110">
-                                          <img 
-                      src="/images/Emmanuel Smith.jpg" 
-                      alt="Emanuel Smith Jr" 
-                      className="w-full h-full object-cover object-center"
-                      style={{ objectPosition: 'center -15%' }}
-                    />
+                                                                <img 
+                        src="/images/Emmanuel Smith.jpg" 
+                        alt="Emanuel Smith Jr" 
+                        className="w-full h-full object-cover object-center"
+                        style={{ objectPosition: 'center -25%' }}
+                      />
                     </div>
                     
                     <div className="flex-1 text-center md:text-left">
@@ -505,7 +505,7 @@ const Index = () => {
                         src="/images/Emmanuel Smith.jpg" 
                         alt="Emanuel A. Smith Jr" 
                         className="w-full h-full object-cover object-center"
-                        style={{ objectPosition: 'center -15%' }}
+                        style={{ objectPosition: 'center -25%' }}
                       />
                     </div>
                     
